dd4b4d2f2bb233cfecf61bde64314fd7529c3f04
your_username <your_email@example.com>
2024-05-27 18:36:38 +0800

    fix FastAPI

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import *
 from fastapi.responses import FileResponse
-app=fastapi()
+app=FastAPI()
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
