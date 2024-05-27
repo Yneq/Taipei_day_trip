239a13bd6c5213e83dd3b6dc0a6ff0405393258b
your_username <your_email@example.com>
2024-05-27 18:20:12 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import *
+from fastapi.responses import FileResponse
+app=fastapi()
+
+# Static Pages (Never Modify Code in this Block)
+@app.get("/", include_in_schema=False)
+async def index(request: Request):
+	return FileResponse("./static/index.html", media_type="text/html")
+@app.get("/attraction/{id}", include_in_schema=False)
+async def attraction(request: Request, id: int):
+	return FileResponse("./static/attraction.html", media_type="text/html")
+@app.get("/booking", include_in_schema=False)
+async def booking(request: Request):
+	return FileResponse("./static/booking.html", media_type="text/html")
+@app.get("/thankyou", include_in_schema=False)
+async def thankyou(request: Request):
+	return FileResponse("./static/thankyou.html", media_type="text/html")
