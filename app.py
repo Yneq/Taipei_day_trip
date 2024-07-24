5df9d494f5389f5472092e0255b13f5ee53711b3
yneq <yneq@ms53.hinet.net>
2024-07-25 05:32:35 +0800

    set websocket s3 rds cloudfront

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from views import static_pages
-from controllers import user_controller, attraction_controller, booking_controller, order_controller
+from controllers import user_controller, attraction_controller, booking_controller, order_controller, pic_controller
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://vancenomad.life", "http://www.vancenomad.life", "https://vancenomad.life"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,3 +23,4 @@ app.include_router(user_controller.router)
 app.include_router(attraction_controller.router)
 app.include_router(booking_controller.router)
 app.include_router(order_controller.router)
+app.include_router(pic_controller.router)
